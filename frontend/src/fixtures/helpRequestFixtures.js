97ca9078e91b5a13d86cc9a773db4451e96b9afd
Wesley Chiba <wchiba@ucsb.edu>
2024-05-03 17:31:52 -0700

    wc - add fixtures

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,43 @@
+const helpRequestFixtures = {
+    oneHelpRequest: {
+        "id": 1,
+        "requesterEmail": "mario@mushroomkingdom.gov",
+        "teamId": "5pm-6",
+        "tableOrBreakoutRoom": "14",
+        "requestTime": "2022-01-03T00:00:00",
+        "explanation": "Mama mia",
+        "solved": false
+    },
+    threeDates: [
+        {
+            "id": 1,
+            "requesterEmail": "mario@mushroomkingdom.gov",
+            "teamId": "5pm-6",
+            "tableOrBreakoutRoom": "14",
+            "requestTime": "2022-01-03T00:00:00",
+            "explanation": "Mama mia",
+            "solved": false
+        },
+        {
+            "id": 2,
+            "requesterEmail": "bowser@mushroomkingdom.gov",
+            "teamId": "6pm-5",
+            "tableOrBreakoutRoom": "41",
+            "requestTime": "2023-01-03T00:00:00",
+            "explanation": "mario will not leave my residence",
+            "solved": false
+        },
+        {
+            "id": 3,
+            "requesterEmail": "luigi@mushroomkingdom.gov",
+            "teamId": "11pm-2",
+            "tableOrBreakoutRoom": "742",
+            "requestTime": "2024-01-03T00:00:00",
+            "explanation": "ran out of spaghetti",
+            "solved": false
+        },
+    ]
+};
+
+
+export { helpRequestFixtures };
