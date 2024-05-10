9715c03f1615be67f3c3591a905a3eb125bd22e4
Anika Misra <amisra@ucsb.edu>
2024-05-10 11:15:35 -0700

    Front End is working, no back end utilities yet - am

---
+++ frontend/src/stories/components/RecommendationRequests/RecommendationRequestsTable.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import RecommendationRequestsTable from "main/components/RecommendationRequests/RecommendationRequestsTable";
+import { recommendationRequestFixtures } from 'fixtures/recommendationRequestFixtures';
+import { currentUserFixtures } from 'fixtures/currentUserFixtures';
+import { rest } from "msw";
+
+export default {
+    title: 'components/RecommendationRequests/RecommendationRequestsTable',
+    component: RecommendationRequestsTable
+};
+
+const Template = (args) => {
+    return (
+        <RecommendationRequestsTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    requests: []
+};
+
+export const ThreeItemsOrdinaryUser = Template.bind({});
+
+ThreeItemsOrdinaryUser.args = {
+    requests: recommendationRequestFixtures.threeRequests,
+    currentUser: currentUserFixtures.userOnly,
+};
+
+export const ThreeItemsAdminUser = Template.bind({});
+ThreeItemsAdminUser.args = {
+    requests: recommendationRequestFixtures.threeRequests,
+    currentUser: currentUserFixtures.adminUser,
+}
+
+ThreeItemsAdminUser.parameters = {
+    msw: [
+        rest.delete('/api/recommendationrequests', (req, res, ctx) => {
+            window.alert("DELETE: " + JSON.stringify(req.url));
+            return res(ctx.status(200),ctx.json({}));
+        }),
+    ]
+};
+
