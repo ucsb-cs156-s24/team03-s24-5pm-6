c9f62d9e768405368c6483d8da0930cd6ea434c5
diyaparm <diyaparmar@ucsb.edu>
2024-05-14 11:28:35 -0700

    hw5 passes test

---
+++ src/test/java/edu/ucsb/cs156/example/web/ArticlesWebIT.java
@@ -0,0 +1,55 @@
+package edu.ucsb.cs156.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import edu.ucsb.cs156.example.WebTestCase;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ArticlesWebIT extends WebTestCase {
+    @Test
+    public void admin_user_can_create_delete_articles() throws Exception {
+        setupUser(true);
+
+        page.getByText("Articles").click();
+
+        page.getByText("Create Article").click();
+        assertThat(page.getByText("Create New Article")).isVisible();
+        page.getByTestId("ArticlesForm-title").fill("Test Article");
+        page.getByTestId("ArticlesForm-url").fill("testing.com");
+        page.getByTestId("ArticlesForm-explanation").fill("I am testing this.");
+        page.getByTestId("ArticlesForm-email").fill("test@gmail.com");
+  
+        page.getByTestId("ArticlesForm-dateAdded").fill("1970-01-01T00:00");
+
+
+        page.getByTestId("ArticlesForm-submit").click();
+
+        assertThat(page.getByTestId("ArticlesTable-cell-row-0-col-explanation"))
+                .hasText("I am testing this.");
+
+        page.getByTestId("ArticlesTable-cell-row-0-col-Delete-button").click();
+
+        assertThat(page.getByTestId("ArticlesTable-cell-row-0-col-name")).not().isVisible();
+    }
+
+    @Test
+    public void regular_user_cannot_create_article() throws Exception {
+        setupUser(false);
+
+        page.getByText("Articles").click();
+
+        assertThat(page.getByText("Create Article")).not().isVisible();
+        assertThat(page.getByTestId("ArticlesTable-cell-row-0-col-name")).not().isVisible();
+    }
+}
