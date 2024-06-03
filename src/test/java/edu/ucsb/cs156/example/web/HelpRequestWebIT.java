57861c6431261177a3fabc21e67802a0c2948362
Wesley Chiba <chibawesley@gmail.com>
2024-06-02 23:25:48 -0700

    wc - add integration tests

---
+++ src/test/java/edu/ucsb/cs156/example/web/HelpRequestWebIT.java
@@ -0,0 +1,66 @@
+package edu.ucsb.cs156.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+import edu.ucsb.cs156.example.WebTestCase;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class HelpRequestWebIT extends WebTestCase {
+    @Test
+    public void admin_user_can_create_edit_delete_helprequest() throws Exception {
+        setupUser(true);
+
+        page.getByText("HelpRequest").click();
+
+        page.getByText("Create Help Request").click();
+        assertThat(page.getByText("Create New HelpRequest")).isVisible();
+        page.getByTestId("HelpRequestForm-requesterEmail").fill("wchiba@ucsb.edu");
+        page.getByTestId("HelpRequestForm-teamId").fill("1pm-6");
+        page.getByTestId("HelpRequestForm-tableOrBreakoutRoom").fill("14");
+        page.getByTestId("HelpRequestForm-requestTime").fill("2024-05-05T08:30");
+        page.getByTestId("HelpRequestForm-explanation").fill("explanation");
+        page.getByTestId("HelpRequestForm-solved").click();
+        page.getByTestId("HelpRequestForm-submit").click();
+
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail"))
+                .hasText("wchiba@ucsb.edu");
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-teamId"))
+                .hasText("1pm-6");
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-tableOrBreakoutRoom"))
+                .hasText("14");
+
+        page.getByTestId("HelpRequestTable-cell-row-0-col-Edit-button").click();
+        assertThat(page.getByText("Edit Help Request")).isVisible();
+        page.getByTestId("HelpRequestForm-requesterEmail").fill("bowser@ucsb.edu");
+        page.getByTestId("HelpRequestForm-teamId").fill("3pm-2");
+        page.getByTestId("HelpRequestForm-submit").click();
+
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-requesterEmail")).hasText("bowser@ucsb.edu");
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-teamId")).hasText("3pm-2");
+
+        page.getByTestId("HelpRequestTable-cell-row-0-col-Delete-button").click();
+
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-name")).not().isVisible();
+    }
+
+    @Test
+    public void regular_user_cannot_create_restaurant() throws Exception {
+        setupUser(false);
+
+        page.getByText("HelpRequest").click();
+
+        assertThat(page.getByText("Create Help Request")).not().isVisible();
+        assertThat(page.getByTestId("HelpRequestTable-cell-row-0-col-name")).not().isVisible();
+    }
+}
